379db29bc37f08fadeb630b3fdf9aadde7c77951
Aleksandr N. Ryzhov <a.n.ryzhov@gmail.com>
2020-07-08 00:18:15 +0300

    handle signal in application

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+
+const logger = new Logger('Application', false);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(3000);
+  return app;
 }
-bootstrap();
+
+bootstrap().then(app => {
+  const process = require('process');
+  const shutdown = (message: string) => app.close().then(_ => logger.log(message));
+
+  process.on('SIGINT', () => shutdown('Exit on SIGINT...'));
+  process.on('SIGTERM', () => shutdown('Exit on SIGTERM...'));
+
+  logger.log('Application started.');
+});
