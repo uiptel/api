99e914faa07c1cbd897498f3eb324e6f3bd587db
Aleksandr N. Ryzhov <a.n.ryzhov@gmail.com>
2020-07-20 17:23:36 +0000

    update logs format

--- src/logger.middleware.ts
+++ src/logger.middleware.ts
@@ -7,8 +7,10 @@ export const logger = new Logger('http', false);
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: Function) {
-    const { ip, protocol, hostname, method, originalUrl: url  } = req;
-    logger.log(`${ip}: ${method} ${protocol}://${hostname} ${url}`);
+    const { ip, method, originalUrl: url  } = req;
+    const { statusCode } = res;
+    const hostname = require('os').hostname();
+    logger.log(`${hostname}: "${method} ${url}" ${statusCode} ${ip}`);
     next();
   }
 }
