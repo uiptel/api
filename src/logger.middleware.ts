2a307d0a741bcd51f2ba5a17b54723fd3ba050be
Aleksandr N. Ryzhov <a.n.ryzhov@gmail.com>
2020-07-23 17:29:49 +0000

    update logger middleware

--- src/logger.middleware.ts
+++ src/logger.middleware.ts
@@ -8,9 +8,16 @@ export const logger = new Logger('http', false);
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: Function) {
     const { ip, method, originalUrl: url  } = req;
-    const { statusCode } = res;
     const hostname = require('os').hostname();
-    logger.log(`${hostname}: "${method} ${url}" ${statusCode} ${ip}`);
+    const userAgent = req.get('user-agent') || '';
+    const referer = req.get('referer') || '';
+
+    res.on('close', () => {
+      const { statusCode, statusMessage } = res;
+      const contentLength = res.get('content-length');
+      logger.log(`[${hostname}] "${method} ${url}" ${statusCode} ${statusMessage} ${contentLength} "${referer}" "${userAgent}" "${ip}"`);
+    });
+
     next();
   }
 }
