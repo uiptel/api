e1c3d2954d9dec165535f4bebb793c0d6ba99e38
Aleksandr N. Ryzhov <a.n.ryzhov@gmail.com>
2020-09-21 00:42:26 +0300

    upd e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import { AppModule } from '../src/app.module';
+import * as request from 'supertest';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -18,10 +19,9 @@ describe('AppController (e2e)', () => {
     expect(app).toBeDefined();
   });
 
-  /*it('/ (GET)', () => {
+  it('/auth/login (POST)', () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });*/
+      .post('/auth/login')
+      .expect(201);
+  });
 });
