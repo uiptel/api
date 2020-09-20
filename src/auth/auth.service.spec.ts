b0f3485488e64ea31ad0cb9aa8caf1eec02d1fcd
Aleksandr N. Ryzhov <a.n.ryzhov@gmail.com>
2020-09-20 15:45:46 +0000

    update for tests

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,4 +1,6 @@
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { UsersModule } from '../users/users.module';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
@@ -6,7 +8,8 @@ describe('AuthService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [AuthService, JwtService],
+      imports: [UsersModule],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
--- src/stat/stat.controller.spec.ts
+++ src/stat/stat.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StatController } from './stat.controller';
 import { StatService } from './stat.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 describe('StatController', () => {
   let statController: StatController;
@@ -9,8 +10,13 @@ describe('StatController', () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [StatController],
       providers: [StatService],
+      imports: [TypeOrmModule],
     }).compile();
 
     statController = app.get<StatController>(StatController);
+  });
+
+  it('should be defined', () => {
+    expect(statController).toBeDefined();
   });
 });
