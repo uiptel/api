30e1a3e73c5a775617642ba087870a87864553c6
Aleksandr N. Ryzhov <a.n.ryzhov@gmail.com>
2020-10-09 22:42:45 +0300

    build and push workflow

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
 VCS_REF=$(git log --format="%H" -n 1)
 VERSION=$(cat package.json | jq -r '.version')
-IMAGE=${REGISTRY}/${SERVICE}_${NAMESPACE}:${VERSION}
+export IMAGE=${REGISTRY}/${SERVICE}_${NAMESPACE}:${VERSION}
 
 docker build -f .docker/Dockerfile -t ${IMAGE} --rm \
     --build-arg WORKDIR=$APP_PATH} \
